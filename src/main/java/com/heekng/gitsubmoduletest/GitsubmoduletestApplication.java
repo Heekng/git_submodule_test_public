23786e28e7c1ec4f06b486bb7bf28394f7b22c4b
heekng <rhkd1769@gmail.com>
2022-03-22 13:39:23 +0900

    initial commit

---
+++ src/main/java/com/heekng/gitsubmoduletest/GitsubmoduletestApplication.java
@@ -0,0 +1,13 @@
+package com.heekng.gitsubmoduletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsubmoduletestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitsubmoduletestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heekng/gitsubmoduletest/GitsubmoduletestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heekng.gitsubmoduletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsubmoduletestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
